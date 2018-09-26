0f901338f842e82a3793ae5cf0b0a7b4271e9b59
Raúl Enrique Reza del Castillo <32489142+BiggerCrayonZ@users.noreply.github.com>
2018-09-25 23:22:59 -0500

    pushDatatoDataSet

--- w_analytics_be/src/model/dataset/functions.js
+++ w_analytics_be/src/model/dataset/functions.js
@@ -1,8 +1,36 @@
 const axios = require('axios');
 const fs = require('fs');
 
+const constants = require('../../../constants');
+
+/* Vars */
+let WATSON_URL = constants.constants.WATSON_AMALYTICS_API_URL;
+let WATSON_PATH = constants.constants.WATSON_ANALYTICS_API_BASE_PATH;
+let APPLICATION_URL = constants.constants.APPLICATION_URL;
+let PORT = constants.constants.PORT;
+
+let appKey = JSON.parse(fs.readFileSync('./appkey.json', 'utf8'));
+let access_token = JSON.parse(fs.readFileSync('./tokens/token.json', 'utf8'));
+let redirect_url = 'http://' + APPLICATION_URL + ':' + PORT + '/oauth2/code';
+
 class datasetFunctions {
     constructor() { }
 
-    pushDatatoDataSet(name, dataset, client_id, client_secret, redirect_url)
+    pushDatatoDataSet(name, dataset) {
+        return new Promise((resolve, reject) => {
+            let date = new Date();
+            let request = 'https://' + WATSON_URL + WATSON_PATH + '/data/v1/datasets';
+            let body = { name: name + date.toISOString() };
+            let id_dataset_options = {
+                headers: {
+                    'X-IBM-Client-Id': appKey.client_id,
+                    'X-IBM-Client-Secret': appKey.client_secret,
+                    'Authorization': 'Bearer ' + access_token.token,
+                    'Content-Type': 'application/json'
+                },
+                data: body
+            };
+            //axios
+        });
+    }
 }
