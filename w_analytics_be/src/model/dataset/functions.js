17c4bd31489e6ce017b2784a733207a31ef3375f
Raúl Reza <raul.reza@ibm.com>
2018-09-26 16:37:49 -0500

    apload and append function is ready

--- w_analytics_be/src/model/dataset/functions.js
+++ w_analytics_be/src/model/dataset/functions.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
+var https = require('https');
 
 const constants = require('../../../constants');
 
@@ -10,38 +11,114 @@ let APPLICATION_URL = constants.constants.APPLICATION_URL;
 let PORT = constants.constants.PORT;
 
 let appKey = JSON.parse(fs.readFileSync('./appkey.json', 'utf8'));
-let access_token = JSON.parse(fs.readFileSync('./tokens/token.json', 'utf8'));
+let access_token;
 let redirect_url = 'http://' + APPLICATION_URL + ':' + PORT + '/oauth2/code';
 
 class datasetFunctions {
-    constructor() { }
+    constructor(access_token) {
+        this.access_token = access_token;
+    }
 
     pushDatatoDataSet(name, dataset) {
+        console.log("access_token: ", this.access_token);
         return new Promise((resolve, reject) => {
-            let date = new Date();
-            let request = 'https://' + WATSON_URL + WATSON_PATH + '/data/v1/datasets';
-            let body = { name: name + date.toISOString() };
-            let id_dataset_options = {
-                headers: {
-                    'X-IBM-Client-Id': appKey.client_id,
-                    'X-IBM-Client-Secret': appKey.client_secret,
-                    'Authorization': 'Bearer ' + access_token.token,
-                    'Content-Type': 'application/json'
-                },
-                data: body
-            };
-            console.log("request: ", request);
-            console.log("body: ", body);
-            console.log("id_dataset_options: ", id_dataset_options);
-            axios.post(request, body, id_dataset_options).then((data) => {
-                let responseObject = data.data;
-            })
+            try {
+                let date = new Date();
+                let request = 'https://' + WATSON_URL + WATSON_PATH + '/data/v1/datasets';
+                let body = { name: name + date.toISOString() };
+                let id_dataset_options = {
+                    headers: {
+                        'X-IBM-Client-Id': appKey.client_id,
+                        'X-IBM-Client-Secret': appKey.client_secret,
+                        'Authorization': 'Bearer ' + this.access_token,
+                        'Content-Type': 'application/json'
+                    },
+                    data: body
+                };
+                console.log("request: ", request);
+                console.log("body: ", body);
+                console.log("id_dataset_options: ", id_dataset_options);
+                axios.post(request, body, id_dataset_options).then((data) => {
+                    let responseObject = data.data;
+                    this.dataSetRequest(responseObject.id, dataset).then((result) => {
+                        let json = {
+                            responseObject: responseObject
+                        }
+                        resolve(json);
+                    }).catch((err) => {
+                        reject(err);
+                    });
+                    resolve(responseObject);
+                })
+            } catch (err) {
+                console.log(err);
+                reject(err);
+            }
         });
     }
 
-    dataSetRequest(dataset_id, dataset, res) {
-        let request = 'http://' + WATSON_URL + WATSON_PATH + '/data/v1/datasets/' + dataset_id + '/content';
-        //let
+    dataSetRequest(dataset_id, dataset) {
+        return new Promise((resolve, reject) => {
+            try {
+                console.log("id: ", dataset);
+                let request_dataset = 'https://' + WATSON_URL + WATSON_PATH + '/data/v1/datasets/' + dataset_id + '/content';
+                let body_dataset = dataset;
+                let data_options = {
+                    headers: {
+                        'X-IBM-Client-Id': appKey.client_id,
+                        'X-IBM-Client-Secret': appKey.client_secret,
+                        'Authorization': 'Bearer ' + this.access_token,
+                        'Content-Type': 'text/csv'
+                    },
+                    data: body_dataset
+                };
+                axios.put(request_dataset, body_dataset, data_options).then((result) => {
+                    let responseString = result.data;
+                    let response = {
+                        responseString: responseString
+                    }
+                    resolve(response);
+                }).catch((errRequest) => {
+                    console.log("err dataSetRequest: ", errRequest);
+                    reject(errRequest);
+                });
+            } catch (err) {
+                console.log("err dataSet: ", err);
+            }
+        })
+
+    };
+
+    pushData(id, appendData) {
+        return new Promise((resolve, reject) => {
+            try {
+                let request_dataset = 'https://' + WATSON_URL + WATSON_PATH + '/data/v1/datasets/' + id + '/content?appendData=true';
+                let body_dataset = appendData;
+                let data_options = {
+                    headers: {
+                        'X-IBM-Client-Id': appKey.client_id,
+                        'X-IBM-Client-Secret': appKey.client_secret,
+                        'Authorization': 'Bearer ' + this.access_token,
+                        'Content-Type': 'text/csv'
+                    }
+                };
+                axios.put(request_dataset, body_dataset, data_options).then((result) => {
+                    let responseString = result.data;
+                    let response = {
+                        responseString: responseString
+                    }
+                    resolve(response);
+                }).catch((errRequest) => {
+                    console.log("err dataSetRequest: ", errRequest);
+                    reject(errRequest);
+                });
+            } catch (err) {
+                console.log("err dataSet: ", err);
+            }
+        })
     }
+
+    //appKey Json
+
 }
 exports.datasetFunctions = datasetFunctions;
--- w_analytics_be/src/routes/analytics.routes.js
+++ w_analytics_be/src/routes/analytics.routes.js
@@ -3,6 +3,7 @@ const constants = require('../../constants');
 const fs = require('fs');
 var opn = require('opn');
 const urlutil = require('url');
+const csv = require("fast-csv");
 
 const routes = require('express').Router();
 const { authFunctions } = require('../model/auth/functions');
@@ -10,7 +11,7 @@ const { datasetFunctions } = require('../model/dataset/functions');
 
 /* Functions */
 let auth = new authFunctions;
-let dataset = new datasetFunctions;
+
 
 /* Vars */
 let WATSON_URL = constants.constants.WATSON_AMALYTICS_API_URL;
@@ -22,6 +23,18 @@ let access_tokens = {
     userID: 'undefined',
     token: 'undefined'
 }
+
+let access_token;
+
+
+if (evaluateToken()) {
+    access_token = JSON.parse(fs.readFileSync('./tokens/token.json', 'utf8'));
+} else {
+    access_token = undefined;
+}
+
+let dataset = new datasetFunctions(access_token.access_token);
+
 let appKey = JSON.parse(fs.readFileSync('./appkey.json', 'utf8'));
 let redirect_url = 'http://' + APPLICATION_URL + ':' + PORT + '/oauth2/code';
 
@@ -49,19 +62,31 @@ routes.route('/oauth2/code').get((req, res) => {
 });
 
 routes.route('/dataset/empty').post((req, res) => {
-    dataset.pushDatatoDataSet('Holiii', {dataset: 'data'}).then(() => {
-        res.sendStatus(200);
+    var body = 'c1, c2\n';
+    body += 'r1, r1\n';
+    body += 'r3, r3\n';
+    dataset.pushDatatoDataSet('30 dataset', body).then((responseObject) => {
+        res.status(200).send(responseObject);
     }).catch((err) => {
         res.status(400).send(err);
     })
+
 });
 
 routes.route('/dataset/upload').post((req, res) => {
-
+    let id = req.body.id;
+    var body = 'c1, c2\n';
+    body += 'r1, r1\n';
+    body += 'r2, r2\n';
+    dataset.pushData(id, body).then((result) => {
+        res.status(200).send(result);
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
 });
 
-routes.route('/dataset/status/:id').get((req, res) => {
-
+routes.route('/dataset/output/:id').get((req, res) => {
+    
 });
 
 routes.route('/dataset/delete/:id').get((req, res) => {
@@ -79,6 +104,16 @@ function setAccessToken(token) {
     access_tokens.userID = 'demo-user';
     access_tokens.token = token;
 };
+
+function evaluateToken() {
+    if (fs.existsSync('../../tokens/token.json')) {
+        console.log("--- false")
+        return false;
+    } else {
+        console.log("--- true")
+        return true;
+    }
+}
 
 /* Export */
 module.exports = routes;
